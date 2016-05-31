e2966dbfe67e0381e15654030b83a64b8c1a8399
tonygj <tonygj@stud.ntnu.no>
2016-06-01 09:30:48 +1000

    Love git

---
+++ BrewersHelper/BrewersHelper/Views/Helper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BrewersHelper
+{
+	public class Helper
+	{
+		public string HelperName{ get; set; }
+		public int Percent;
+
+		public Helper (string helperName, int percent)
+		{
+			HelperName = helperName;
+			Percent = percent;
+		}
+	}
+}
+
